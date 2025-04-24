c7bd36e9402ddb4405b36456c146c9f166663b12
ssckp <pemmasaniswathi@gmail.com>
2025-04-24 19:51:37 +0530

    we push into central

--- src/GitProject/swathi.java
+++ src/GitProject/swathi.java
@@ -1,5 +1,14 @@
 package GitProject;
 
 public class swathi {
+	public static void main(String[]args)
+	{
+		int x=100;int y=200;
+		int temp=x;
+		x=y;
+		y=temp;
+		System.out.println("x :" + x);
+		
+	}
 
 }
